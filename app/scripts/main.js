d7bd8cf0531260ecedeb01a0e3159ebba6b06531
schnubor <info@chko.org>
2017-06-10 14:29:35 +0200

    border animation and flipped content

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,6 +1,18 @@
 'use strict';
 $( document ).ready( function(){
-    $('.main-border').click( function() {
+    $('body').on( 'click', '.main-border.clickable', function() {
         $(this).toggleClass( 'active' );
+        $(this).removeClass( 'clickable' );
+
+        setTimeout( function() {
+            $('.content-wrapper').fadeIn();
+        }, 1000 ); // 1s CSS transition time
     } );
+} );
+
+$(window).on('load', function() {
+    setTimeout( function() {
+        $('.main-border .spinner').fadeOut();
+        $('.main-border').addClass('clickable');
+    }, 1000 );
 } );
