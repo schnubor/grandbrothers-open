4ad482e889e25af6b0521e98a992bd0903f170ed
schnubor <info@chko.org>
2017-06-11 13:20:33 +0200

    added scroll lib and bg animation

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,13 +1,25 @@
 'use strict';
+
+var scrollValue = 0;
+
 $( document ).ready( function(){
     $('body').on( 'click', '.main-border.clickable', function() {
-        $(this).toggleClass( 'active' );
+        $(this).addClass( 'active' );
         $(this).removeClass( 'clickable' );
 
         setTimeout( function() {
             $('.content-wrapper').fadeIn();
+            $('body').mousewheel(function(event) {
+                scrollValue += event.deltaY;
+                if( scrollValue < 0 ) scrollValue = 0;
+                console.log( scrollValue );
+
+                // update bg positions
+                $('.left, .right').css('backgroundPosition', '-' + scrollValue * 0.2 + 'px 0px');
+            });
         }, 1000 ); // 1s CSS transition time
     } );
+
 } );
 
 $(window).on('load', function() {
@@ -16,3 +28,4 @@ $(window).on('load', function() {
         $('.main-border').addClass('clickable');
     }, 1000 );
 } );
+
